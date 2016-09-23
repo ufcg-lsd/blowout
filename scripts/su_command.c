a3f151d5676a3579bf96a3211d0846e95f11e44e
Thiago Manel <thiago.manel@gmail.com>
2016-09-23 15:32:01 -0300

    add a small readme and change the name of the program

--- scripts/su_command.c
+++ scripts/su_command.c
@@ -4,24 +4,23 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
+  //Usage: $0 uid gid commard arg0 .. argn
   int i;
   char *buffer;
 
   buffer = malloc(1024);
 
   buffer[0] = '\0';
- 
-  if (argc < 4) exit(0);
+
+  if (argc < 4) exit(1);
 
   for (i = 3; i < argc; i++) sprintf(buffer, "%s %s", buffer, argv[i]);
-
-  printf("%s\n", buffer);
 
   int suid = atoi(argv[1]);
   setuid(suid);
   int sgid = atoi(argv[2]);
   setgid(sgid);
   int status = system(buffer);
-
+  return status;
 }
 
