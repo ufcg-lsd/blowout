40a09c28f431f73ca325030746ef242e61e44624
EsdrasVP <esdras015@gmail.com>
2017-05-23 14:33:59 -0300

    Adding more log to debug

--- src/main/java/org/fogbowcloud/blowout/infrastructure/manager/DefaultInfrastructureManager.java
+++ src/main/java/org/fogbowcloud/blowout/infrastructure/manager/DefaultInfrastructureManager.java
@@ -5,7 +5,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.concurrent.ConcurrentHashMap;
 
+import org.apache.log4j.Logger;
 import org.fogbowcloud.blowout.core.model.Specification;
 import org.fogbowcloud.blowout.core.model.Task;
 import org.fogbowcloud.blowout.core.model.TaskState;
@@ -13,7 +15,6 @@ import org.fogbowcloud.blowout.infrastructure.model.ResourceState;
 import org.fogbowcloud.blowout.infrastructure.monitor.ResourceMonitor;
 import org.fogbowcloud.blowout.infrastructure.provider.InfrastructureProvider;
 import org.fogbowcloud.blowout.pool.AbstractResource;
-import org.fogbowcloud.blowout.pool.BlowoutPool;
 
 public class DefaultInfrastructureManager implements InfrastructureManager {
 
@@ -22,6 +23,8 @@ public class DefaultInfrastructureManager implements InfrastructureManager {
 
 	// private Map<Task, AbstractResource> allocatedResources = new
 	// ConcurrentHashMap<Task, AbstractResource>();
+	private Map<Task, AbstractResource> taskResourceMap = new ConcurrentHashMap<Task, AbstractResource>();
+	private static final Logger LOGGER = Logger.getLogger(DefaultInfrastructureManager.class);
 
 	public DefaultInfrastructureManager(InfrastructureProvider infraProvider, ResourceMonitor resourceMonitor) {
 		this.infraProvider = infraProvider;
@@ -31,9 +34,11 @@ public class DefaultInfrastructureManager implements InfrastructureManager {
 	@Override
 	public synchronized void act(List<AbstractResource> resources, List<Task> tasks) throws Exception {
 
+		LOGGER.debug("Calling DefaultInfrastructureManager act");
 		Map<Specification, Integer> specsDemand = new HashMap<Specification, Integer>();
 		List<AbstractResource> idleResources = filterResourcesByState(resources, ResourceState.IDLE);
-		
+		LOGGER.debug("idleResources=" + idleResources.size());
+				
 		// Generate demand for tasks
 		for (Task task : tasks) {
 			if (!task.isFinished()) {
@@ -50,7 +55,8 @@ public class DefaultInfrastructureManager implements InfrastructureManager {
 					incrementDecrementDemand(specsDemand, task.getSpecification(), true);
 				} else {
 					idleResources.remove(resourceToRemove);
-				}
+				}				
+				LOGGER.debug("resourceResolved=" + resourceResolved + " task=" + task.getId());
 			}
 		}
 
@@ -58,17 +64,18 @@ public class DefaultInfrastructureManager implements InfrastructureManager {
 		for (Specification pendingSpec : resourceMonitor.getPendingSpecification()) {
 			incrementDecrementDemand(specsDemand, pendingSpec, false);
 		}
+		
+		LOGGER.debug("specsDemand=" + specsDemand.size());
+		LOGGER.debug("pendingSpecification=" + resourceMonitor.getPendingSpecification().size());
 
-		// Request resources according to the demand.
+		// Request resources according to demand.
 		for (Entry<Specification, Integer> entry : specsDemand.entrySet()) {
-
 			Specification spec = entry.getKey();
 			Integer qty = entry.getValue();
 			for (int count = 0; count < qty.intValue(); count++) {
-
 				String resourceId = infraProvider.requestResource(spec);
 				resourceMonitor.addPendingResource(resourceId, spec);
-
+				LOGGER.debug("Adding resource " + resourceId + " to pendingSpecification");
 			}
 		}
 	}
