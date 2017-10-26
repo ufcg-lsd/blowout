8c6d06e51d18be5de63e3f29a231eff4054a1117
fgan1 <fgan1@hotmail.com>
2017-10-26 19:40:40 -0300

    Implementing NAFTokenUpdatePLugin

--- src/main/java/org/fogbowcloud/blowout/infrastructure/token/NAFTokenUpdatePlugin.java
+++ src/main/java/org/fogbowcloud/blowout/infrastructure/token/NAFTokenUpdatePlugin.java
@@ -1,136 +1,120 @@
 package org.fogbowcloud.blowout.infrastructure.token;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.KeyFactory;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Date;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
+import org.apache.log4j.Logger;
+import org.bouncycastle.util.encoders.Base64;
 import org.fogbowcloud.blowout.core.exception.BlowoutException;
 import org.fogbowcloud.blowout.core.util.AppPropertiesConstants;
 import org.fogbowcloud.manager.core.plugins.identity.naf.NAFIdentityPlugin;
+import org.fogbowcloud.manager.core.plugins.identity.naf.RSAUtils;
 import org.fogbowcloud.manager.occi.model.Token;
+
+import com.google.gson.JsonObject;
 
 public class NAFTokenUpdatePlugin extends AbstractTokenUpdatePlugin {
 	
-	private static final int DEFAULT_UPDATE_TIME = 6;
-	private static final TimeUnit DEFAULT_UPDATE_TIME_UNIT = TimeUnit.HOURS;
-	private static final String USERNAME_PARAMETER = "username";
-	private static final String PASSWORD_PARAMETER = "password";
-	public static final String NAF_IDENTITY_PRIVATE_KEY = AppPropertiesConstants.INFRA_AUTH_TOKEN_PREFIX
-			+ "naf_identity_private_key";
-	public static final String NAF_IDENTITY_PUBLIC_KEY = AppPropertiesConstants.INFRA_AUTH_TOKEN_PREFIX
-			+ "naf_identity_private_key";
-	public static final String NAF_IDENTITY_TOKEN_USERNAME = AppPropertiesConstants.INFRA_AUTH_TOKEN_PREFIX
-			+ "naf_identity_token_username";
-	public static final String NAF_IDENTITY_TOKEN_PASSWORD = AppPropertiesConstants.INFRA_AUTH_TOKEN_PREFIX
-			+ "naf_identity_token_password";
-	public static final String NAF_IDENTITY_TOKEN_GENERATOR_URL = AppPropertiesConstants.INFRA_AUTH_TOKEN_PREFIX
-			+ "naf_identity_token_generator_endpoint";
-	private static final String HOUR_PARAMETER = "hour";
+	private static final Logger LOGGER = Logger.getLogger(NAFTokenUpdatePlugin.class);
+	
+	private static final String INFRA_AUTH_PREFIX = AppPropertiesConstants.INFRA_AUTH_TOKEN_PREFIX;
+	public static final String TOKEN_PLUGIN_USERNAME = INFRA_AUTH_PREFIX + "naf_username";
+	private static final String TAG_SEPARETOR_DASHBOARD_NAF_AUTH = "!#!";
+	private static final String DEFAULT_NAME = "fogbow_user_naf";
+	
+	private static final String SAML_ATTRIBUTES_KEY_JSON = "saml_attributes";
+	private static final String TOKEN_ETIME_KEY_JSON = "token_etime";
+	private static final String NAME_KEY_JSON = "name";
+	
+	protected static final String MAN_NAF_IDENTITY_PUBLIC_KEY = "naf_identity_public_key";
+	public static final String NAF_IDENTITY_PUBLIC_KEY = INFRA_AUTH_PREFIX + MAN_NAF_IDENTITY_PUBLIC_KEY;
+	protected static final String MAN_NAF_IDENTITY_PRIVATE_KEY = "naf_identity_private_key";
+	public static final String NAF_IDENTITY_PRIVATE_KEY = INFRA_AUTH_PREFIX + MAN_NAF_IDENTITY_PRIVATE_KEY;
 
-	private Properties properties;
-
-	public NAFTokenUpdatePlugin(Properties properties) {
+	
+	public NAFTokenUpdatePlugin(Properties properties) throws BlowoutException {
 		super(properties);
-		this.properties = properties;
+		validateProperties();
 	}
 
 	@Override
 	public Token generateToken() {
+		LOGGER.debug("Creating NAF Token.");
+		Token token = null;
 		try {
+			Properties properties = new Properties();
+			properties.put(MAN_NAF_IDENTITY_PUBLIC_KEY, this.properties.getProperty(NAF_IDENTITY_PUBLIC_KEY));
 			NAFIdentityPlugin nafIdentityPlugin = new NAFIdentityPlugin(properties);
-			String accessId = requestTokenFromGenerator();
-			Token token = nafIdentityPlugin.getToken(accessId);
-			return token;
+			String accessIdMessage = createAccessId();
+			
+			String privateKeyPath = this.properties.getProperty(NAF_IDENTITY_PRIVATE_KEY);
+			RSAPrivateKey privateKey = getPrivateKey(privateKeyPath);
+			String accessIdMessageSigned = RSAUtils.sign(privateKey, accessIdMessage);
+			
+			String accessId = accessIdMessage + TAG_SEPARETOR_DASHBOARD_NAF_AUTH + accessIdMessageSigned;
+			String accessIdEnconded = new String(Base64.encode(accessId.getBytes()));
+			token = nafIdentityPlugin.getToken(accessIdEnconded);
+			LOGGER.info("Token created. " + token.toString());
 		} catch (Exception e) {
-			return null;
+			LOGGER.warn("Error while creating token.", e);
 		}
+		return token;
 	}
-
-	private String requestTokenFromGenerator() {
-		String tokenGeneratorUrl = properties.getProperty(NAF_IDENTITY_TOKEN_GENERATOR_URL);
-		String userName = properties.getProperty(NAF_IDENTITY_TOKEN_USERNAME);
-		String password = properties.getProperty(NAF_IDENTITY_TOKEN_PASSWORD);
-		int hours = getTokenUpdateTimeInHours();
-
-		if (userName == null || password == null || tokenGeneratorUrl == null) {
-			return null;
+	
+	private static String getKey(String filename) throws IOException {
+		// Read key from file
+		String strKeyPEM = "";
+		BufferedReader br = new BufferedReader(new FileReader(filename));
+		String line;
+		while ((line = br.readLine()) != null) {
+			strKeyPEM += line;
 		}
-
-		try {
-			CloseableHttpClient httpClient = HttpClients.createMinimal();
-			HttpPost httpPost = new HttpPost(tokenGeneratorUrl);
-			List<NameValuePair> parameters = new ArrayList<NameValuePair>();
-			parameters.add(new BasicNameValuePair(USERNAME_PARAMETER, userName));
-			parameters.add(new BasicNameValuePair(PASSWORD_PARAMETER, password));
-			parameters.add(new BasicNameValuePair(HOUR_PARAMETER, String.valueOf(hours)));
-			UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(parameters);
-			httpPost.setEntity(formEntity);
-
-			CloseableHttpResponse response = httpClient.execute(httpPost);
-			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-				return IOUtils.toString(response.getEntity().getContent());
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return null;
+		br.close();
+		return strKeyPEM;
 	}
+	
+	private static RSAPrivateKey getPrivateKey(String filename) throws Exception {
+		String privateKeyPEM = getKey(filename);
 
-	private int getTokenUpdateTimeInHours() {
+		// Remove the first and last lines
+		privateKeyPEM = privateKeyPEM
+				.replace("-----BEGIN PRIVATE KEY-----", "");
+		privateKeyPEM = privateKeyPEM.replace("-----END PRIVATE KEY-----", "");
 
-		int hourInMinutes = 60;
-		int hourInSeconds = hourInMinutes * 60;
-		int hourInMiliseconds = hourInSeconds * 1000;
+		// Base64 decode data
+		byte[] encoded = org.bouncycastle.util.encoders.Base64
+				.decode(privateKeyPEM);
 
-		int updateTime = getUpdateTime();
-		TimeUnit updateTimeUnits = getUpdateTimeUnits();
-		if (updateTimeUnits.equals(TimeUnit.MINUTES)) {
-			updateTime = updateTime / hourInMinutes;
-		} else if (updateTimeUnits.equals(TimeUnit.SECONDS)) {
-			updateTime = updateTime / hourInSeconds;
-		} else if (updateTimeUnits.equals(TimeUnit.MILLISECONDS)) {
-			updateTime = updateTime / hourInMiliseconds;
-		}
-
-		return Math.max(1, updateTime);
+		KeyFactory kf = KeyFactory.getInstance("RSA");
+		RSAPrivateKey privKey = (RSAPrivateKey) kf
+				.generatePrivate(new PKCS8EncodedKeySpec(encoded));
+		return privKey;
 	}
-
+	
+	protected String createAccessId() {
+		JsonObject jsonObject = new JsonObject();
+		String name = this.properties.getProperty(TOKEN_PLUGIN_USERNAME);
+		jsonObject.addProperty(NAME_KEY_JSON, name != null ? name: DEFAULT_NAME);
+		long infinitTime = new Date(Long.MAX_VALUE).getTime();
+		jsonObject.addProperty(TOKEN_ETIME_KEY_JSON, String.valueOf(infinitTime));
+		jsonObject.add(SAML_ATTRIBUTES_KEY_JSON, new JsonObject());
+		
+		return jsonObject.toString();
+	}
 
 	@Override
 	public void validateProperties() throws BlowoutException {
-		if (!properties.containsKey(NAF_IDENTITY_PRIVATE_KEY)) {
-			throw new BlowoutException(
-					"Required property " + NAF_IDENTITY_PRIVATE_KEY + " was not set");
-		}
-
 		if (!properties.containsKey(NAF_IDENTITY_PUBLIC_KEY)) {
-			throw new BlowoutException(
-					"Required property " + NAF_IDENTITY_PUBLIC_KEY + " was not set");
+			throw new BlowoutException("Required property " + NAF_IDENTITY_PUBLIC_KEY + " was not set.");
 		}
-
-		if (!properties.containsKey(NAF_IDENTITY_TOKEN_GENERATOR_URL)) {
-			throw new BlowoutException("Required property " + NAF_IDENTITY_TOKEN_GENERATOR_URL
-					+ " was not set");
-		}
-
-		if (!properties.containsKey(NAF_IDENTITY_TOKEN_USERNAME)) {
-			throw new BlowoutException(
-					"Required property " + NAF_IDENTITY_TOKEN_USERNAME + " was not set");
-		}
-
-		if (!properties.containsKey(NAF_IDENTITY_TOKEN_PASSWORD)) {
-			throw new BlowoutException(
-					"Required property " + NAF_IDENTITY_TOKEN_PASSWORD + " was not set");
-		}
+		if (!properties.containsKey(NAF_IDENTITY_PRIVATE_KEY)) {
+			throw new BlowoutException("Required property " + NAF_IDENTITY_PRIVATE_KEY + " was not set.");
+		}		
 	}
 }
